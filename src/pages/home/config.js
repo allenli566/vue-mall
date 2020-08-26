a4634928331eb70c805617859a94e8dbd939095f
allenli556 <244413308@qq.com>
2020-08-26 23:16:16 +0800

    fist

---
+++ src/pages/home/config.js
@@ -0,0 +1,77 @@
+export const sliderOptions = { // 轮播图配置
+  direction: 'horizontal', // horizontal:水平 vertical:垂直
+  loop: true, // 是否无缝滚动
+  interval: 3000, // 自动播放时间间隔，为0则不自动播放
+  pagination: true // 是否需要分页器
+};
+export const HEADER_TRANSITION_HEIGHT = 100;
+
+export const navItems = [
+  
+  {
+    linkUrl: 'javascrip:;',
+    picUrl: require('./img/nav-item-1.png'),
+    text: '包袋'
+  },
+  {
+    linkUrl: 'javascrip:;',
+    picUrl: require('./img/nav-item-2.png'),
+    text: '男装'
+  },
+  {
+    linkUrl: 'javascrip:;',
+    picUrl: require('./img/nav-item-9.png'),
+    text: '女装'
+  },
+  {
+    linkUrl: 'javascrip:;',
+    picUrl: require('./img/nav-item-4.png'),
+    text: '饮料水品'
+  },
+  {
+    linkUrl: 'javascrip:;',
+    picUrl: require('./img/nav-item-5.png'),
+    text: '红酒'
+  },
+  {
+    linkUrl: 'javascrip:;',
+    picUrl: require('./img/nav-item-6.png'),
+    text: '食用油'
+  },
+  {
+    linkUrl: 'javascrip:;',
+    picUrl: require('./img/nav-item-7.png'),
+    text: '清洁用品'
+  },
+  {
+    linkUrl: 'javascrip:;',
+    picUrl: require('./img/nav-item-8.png'),
+    text: '洗刷'
+  },
+  {
+    linkUrl: 'javascrip:;',
+    picUrl: require('./img/nav-item-3.png'),
+    text: '休闲零食'
+  },
+  {
+    linkUrl: 'javascrip:;',
+    picUrl: require('./img/nav-item-10.png'),
+    text: '牛奶'
+  }
+]
+
+export const sliderItems = [
+  {
+    linkUrl: 'javascrip:;',
+    picUrl: require('./img/banner-1.jpg')
+  },
+  {
+    linkUrl: 'javascrip:;',
+    picUrl: require('./img/banner-2.jpg')
+  },
+  {
+    linkUrl: 'javascrip:;',
+    picUrl: require('./img/banner-3.jpg')
+  }
+]
+
